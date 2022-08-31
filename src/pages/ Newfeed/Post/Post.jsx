749ce1650829dda75e8016709c747a2e09d727f0
jungjung261 <jungjung.now@gmail.com>
2022-08-31 21:42:40 +0700

    fix post

--- src/pages/ Newfeed/Post/Post.jsx
+++ src/pages/ Newfeed/Post/Post.jsx
@@ -1,8 +1,6 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import classNames from 'classnames/bind';
-
-import styles from './Post.module.scss';
-import Image from '~/components/Image';
-import catlofi from '~/assets/images/post-image.webp';
+import { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faShare,
@@ -13,22 +11,69 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
+import Menu from '~/components/Popper/Menu';
+import styles from './Post.module.scss';
+import Image from '~/components/Image';
+
 const cx = classNames.bind(styles);
 
-function Post() {
+function Post({
+	avatar,
+	name,
+	faculty,
+	caption,
+	photo,
+	likeCount,
+	commentCount,
+	commentContent,
+}) {
+	const [openComment, setOpenComment] = useState(false);
+	const [openOption, setOpenOption] = useState(false);
+
+	const handleOpenOption = () => {
+		setOpenOption(!openOption);
+	};
+
+	// open comment and focus on comment input
+	const focusCommentInputRef = useRef();
+	const handleClickOpenComment = () => {
+		setOpenComment(!openComment);
+	};
+	useEffect(() => {
+		if (openComment) {
+			focusCommentInputRef.current.focus();
+		}
+	}, [openComment]);
+
 	return (
 		<div className={cx('wrapper')}>
 			<div className={cx('container')}>
 				<div className={cx('heading')}>
-					<Image className={cx('avatar')} src='' />
-					<div className={cx('info')}>
-						<span className={cx('info__name')}>Nguyen Van A</span>
-						<span className={cx('info__facutly')}>Cong nghe thong tin</span>
+					<div className={cx('personally')}>
+						<Image className={cx('avatar')} src={avatar} />
+						<div className={cx('info')}>
+							<span className={cx('info__name')}>{name}</span>
+							<span className={cx('info__facutly')}>{faculty}</span>
+						</div>
 					</div>
+
+					<Menu interactive open={openOption}>
+						<div
+							className={cx('option')}
+							onClick={() => {
+								handleOpenOption();
+							}}
+						>
+							<FontAwesomeIcon icon={faEllipsis} />
+						</div>
+					</Menu>
 				</div>
-				<p className={cx('caption')}>Lorem Ipsum is simply dummy text...</p>
+				{caption && <p className={cx('caption')}>{caption}</p>}
 			</div>
-			<img className={cx('image')} src={catlofi} alt='post-img' />
+
+			<div className={cx('photo')}>
+				{photo && <img src={photo} alt='photo' />}
+			</div>
 
 			<div className={cx('interac')}>
 				<div className={cx('interac__react')}>
@@ -37,10 +82,14 @@ function Post() {
 					</div>
 
 					<div className={cx('interac_react-count')}>
-						<span>12</span>
+						{likeCount > 1 && <span>{likeCount}</span>}
 					</div>
 				</div>
-				<div className={cx('interac__comment-count')}>12 comments</div>
+				{commentCount > 0 && (
+					<div className={cx('interac__comment-count')}>
+						{commentCount} comments
+					</div>
+				)}
 			</div>
 
 			<hr />
@@ -51,7 +100,10 @@ function Post() {
 					<span className={cx('interacting__handle-text')}>Like</span>
 				</div>
 
-				<div className={cx('interacting__handle')}>
+				<div
+					className={cx('interacting__handle')}
+					onClick={handleClickOpenComment}
+				>
 					<FontAwesomeIcon icon={faComment} />
 					<span className={cx('interacting__handle-text')}>Comment</span>
 				</div>
@@ -64,50 +116,49 @@ function Post() {
 
 			<hr />
 
-			<div className={cx('comment')}>
-				<div className={cx('comment__commenting')}>
-					<Image className={cx('avatar')} src='' />
-					<div className={cx('comment__commenting-input')}>
-						<input className={cx('input')} placeholder='Write a comment...' />
-						<div className={cx('icon')}>
-							<FontAwesomeIcon className={cx('icon__item')} icon={faSmile} />
-							<FontAwesomeIcon className={cx('icon__item')} icon={faCamera} />
+			{openComment && (
+				<div className={cx('comment')}>
+					<div className={cx('comment__commenting')}>
+						<Image className={cx('avatar')} src={avatar} />
+						<div className={cx('comment__commenting-input')}>
+							<input
+								ref={focusCommentInputRef}
+								className={cx('input')}
+								placeholder='Write a comment...'
+							/>
+							<div className={cx('icon')}>
+								<FontAwesomeIcon className={cx('icon__item')} icon={faSmile} />
+								<FontAwesomeIcon className={cx('icon__item')} icon={faCamera} />
+							</div>
 						</div>
 					</div>
-				</div>
 
-				{/* Comment user */}
+					{/* Comment user */}
 
-				<div className={cx('comment__section')}>
-					<div>
-						<Image src='' className={cx('avatar')} />
-					</div>
-					<div className={cx('comment__section-content')}>
-						<span className={cx('comment__section-content-name')}>
-							Ton Duc Thang
-						</span>
+					{commentContent && (
+						<div className={cx('comment__section')}>
+							<div>
+								<Image src={avatar} className={cx('avatar')} />
+							</div>
+							<div className={cx('comment__section-content')}>
+								<span className={cx('comment__section-content-name')}>
+									{name}
+								</span>
 
-						<p className={cx('comment__section-content-text')}>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum has been the industry's standard dummy text
-							ever since the 1500s, when an unknown printer took a galley of
-							type and scrambled it to make a type specimen book. It has
-							survived not only five centuries, but also the leap into
-							electronic typesetting, remaining essentially unchanged. It was
-							popularised in the 1960s with the release of Letraset sheets
-							containing Lorem Ipsum passages, and more recently with desktop
-							publishing software like Aldus PageMaker including versions of
-							Lorem Ipsum.
-						</p>
-					</div>
+								<p className={cx('comment__section-content-text')}>
+									{commentContent}
+								</p>
+							</div>
 
-					<div className={cx('menu')}>
-						<div className={cx('menu__icon')}>
-							<FontAwesomeIcon icon={faEllipsis} />
+							<div className={cx('menu')}>
+								<div className={cx('menu__icon')}>
+									<FontAwesomeIcon icon={faEllipsis} />
+								</div>
+							</div>
 						</div>
-					</div>
+					)}
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
