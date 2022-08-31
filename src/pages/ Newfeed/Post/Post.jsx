174612bb992d53f9c0937de34e9184497ff41b58
jungjung261 <jungjung.now@gmail.com>
2022-08-31 15:31:20 +0700

    create post

--- src/pages/ Newfeed/Post/Post.jsx
+++ src/pages/ Newfeed/Post/Post.jsx
@@ -7,6 +7,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faShare,
 	faHeart as heartIcon,
+	faSmile,
+	faCamera,
+	faEllipsis,
 } from '@fortawesome/free-solid-svg-icons';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
@@ -56,6 +59,53 @@ function Post() {
 				<div className={cx('interacting__handle')}>
 					<FontAwesomeIcon icon={faShare} />
 					<span className={cx('interacting__handle-text')}>Share</span>
+				</div>
+			</div>
+
+			<hr />
+
+			<div className={cx('comment')}>
+				<div className={cx('comment__commenting')}>
+					<Image className={cx('avatar')} src='' />
+					<div className={cx('comment__commenting-input')}>
+						<input className={cx('input')} placeholder='Write a comment...' />
+						<div className={cx('icon')}>
+							<FontAwesomeIcon className={cx('icon__item')} icon={faSmile} />
+							<FontAwesomeIcon className={cx('icon__item')} icon={faCamera} />
+						</div>
+					</div>
+				</div>
+
+				{/* Comment user */}
+
+				<div className={cx('comment__section')}>
+					<div>
+						<Image src='' className={cx('avatar')} />
+					</div>
+					<div className={cx('comment__section-content')}>
+						<span className={cx('comment__section-content-name')}>
+							Ton Duc Thang
+						</span>
+
+						<p className={cx('comment__section-content-text')}>
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry. Lorem Ipsum has been the industry's standard dummy text
+							ever since the 1500s, when an unknown printer took a galley of
+							type and scrambled it to make a type specimen book. It has
+							survived not only five centuries, but also the leap into
+							electronic typesetting, remaining essentially unchanged. It was
+							popularised in the 1960s with the release of Letraset sheets
+							containing Lorem Ipsum passages, and more recently with desktop
+							publishing software like Aldus PageMaker including versions of
+							Lorem Ipsum.
+						</p>
+					</div>
+
+					<div className={cx('menu')}>
+						<div className={cx('menu__icon')}>
+							<FontAwesomeIcon icon={faEllipsis} />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
